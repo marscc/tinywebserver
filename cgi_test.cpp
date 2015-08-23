fa8827a595484f7809a523ad8da34b434af0547f
marscc <769034513@qq.com>
2015-08-23 16:02:34 +0800

    Create cgi_test.cpp

---
+++ cgi_test.cpp
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define MAXLINE 256
+
+int main()
+{
+	char *buf, *p;
+	char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+	int n1 = 0, n2 = 0;
+	if((buf = getenv("QUERY_STRING")) != NULL)
+	{
+		p = strchr(buf, '&');
+		*p = '\0';
+		strcpy(arg1, buf);
+		strcpy(arg2, p+1);
+		n1 = atoi(arg1);
+		n2 = atoi(arg2);
+	}
+	sprintf(content, "welcome to add.com: ");
+	sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
+	sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content, n1, n2, n1 + n2);
+	sprintf(content, "%sThanks for visiting!\r\n>", content);
+	
+	printf("HTTP/1.1 200 OK\r\nServer: lebur/1.0\r\n");
+	printf("Content-length: %d\r\n", (int)strlen(content));
+	printf("Content-type: text/html\r\n\r\n");
+	printf("%s", content);
+	fflush(stdout);
+	return 0;
+}
